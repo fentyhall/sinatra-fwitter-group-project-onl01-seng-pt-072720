e93ec717de8e531a218d4625304ce1233f0469cd
fentyhall <fentymhall@gmail.com>
2020-10-28 23:51:05 -0700

    Done.

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -36,8 +36,8 @@ class TweetsController < ApplicationController
     end 
 
     get '/tweets/:id/edit' do 
-        @tweet = Tweet.find_by_id(params[:id])
-        if logged_in? && current_user && (current_user.id == @tweet.user_id)
+        if logged_in? && current_user 
+            @tweet = Tweet.find_by_id(params[:id])
             erb :'tweets/edit_tweet'
         else 
             redirect to '/login'
